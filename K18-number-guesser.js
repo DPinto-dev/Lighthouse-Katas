9eff9af492c3550468805990eb1cf6afbefe2ead
DPintoLL <diogosmp@gmail.com>
2020-06-23 02:11:10 -0700

    Refactored K18

--- K18-number-guesser.js
+++ K18-number-guesser.js
@@ -13,24 +13,34 @@ const prompt = require("prompt-sync")();
 let notAMatch = true;
 let tries = 0;
 const answers = [];
-
 let randomNum = Math.floor(Math.random() * 100 + 1);
 
 while (notAMatch) {
   let answer = prompt("Guess a number between 1 and 100: > ");
-  if (!answers.includes(answer)) tries++;
 
   if (answer === "quit") {
-    process.exit(1);
-  } else if (!answer) {
-    console.log("Type something valid");
+    process.exit(0);
+  } else if (answers.includes(answer)) {
+    console.log(
+      "You have given that answer before, please try again with a different number."
+    );
+
+    // If no answer is given or if it's not a number (excep. of 'quit')
+  } else if (!answer || !parseInt(answer)) {
+    console.log("Please, write a valid number.");
+
+    //Winning condition
   } else if (parseInt(answer) === randomNum) {
+    tries++;
     notAMatch = false;
   } else if (answer > randomNum) {
+    answers.push(answer);
+    tries++;
     console.log(`Too high! Tries: ${tries}`);
   } else if (answer < randomNum) {
+    answers.push(answer);
+    tries++;
     console.log(`Too low! Tries: ${tries}`);
-  } else {
   }
 }
 console.log(`Congrats! You guessed it right, using ${tries} tries`);
